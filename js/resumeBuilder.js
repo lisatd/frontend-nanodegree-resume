6ac493335677013738d3a21d4e426c5ee9f80c65
Lisa Dang <ldang@aptima.com>
2015-10-28 17:04:06 -0400

    add JSONs for resume data

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,62 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    'name': 'Lisa T. Dang',
+    'role': 'Software Engineer II',
+    'contacts': {
+        'mobile': '603-479-8883',
+        'email': 'lisatdang@outlook.com',
+        'github': 'change',
+        'twitter': 'change',
+        'location': 'Boston, MA'
+    },
+    'welcomeMessage': 'change',
+    'skills': ['HTML', 'CSS', 'JavaScript'],
+    'biopic': 'change',
+    'display': function () {
+
+    }
+};
+
+var education = {
+    'schools': [{
+        'name': 'Stony Brook University',
+        'location': 'Stony Brook, NY, USA',
+        'degree': 'B.S. in Computer Science',
+        'majors': ['Computer Science'],
+        'dates': 2013,
+        'url': 'change'
+    }],
+    'onlineCourses': [{
+        'title': 'Nanodegree',
+        'school': 'Udacity',
+        'date': 2016,
+        'url': 'change'
+    }],
+    'display': function() {
+
+    }
+};
+
+var work = {
+    'jobs': [{
+        'employer': 'Aptima, Inc.',
+        'title': 'Software Engineer',
+        'location': 'Woburn, MA',
+        'dates': 'Aug 2013 - Present',
+        'description': 'change'
+    }],
+    'display': function() {
+
+    }
+};
+
+var projects = {
+    'projects': [{
+        'title': 'change',
+        'dates': 'change',
+        'description': 'change',
+        'images': ['change']
+    }],
+    'display': function() {
+
+    }
+};
