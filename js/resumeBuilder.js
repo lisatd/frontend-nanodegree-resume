ec35634d262932a35c6776dba7eeb9e32c6a91be
Lisa Dang <ldang@aptima.com>
2015-10-28 17:47:39 -0400

    remove placeholder test add display function for appending bio data to index change flex direction for skills ul

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,15 +4,48 @@ var bio = {
     'contacts': {
         'mobile': '603-479-8883',
         'email': 'lisatdang@outlook.com',
-        'github': 'change',
-        'twitter': 'change',
+        'github': 'https://github.com/lisatd',
+        'twitter': null,
         'location': 'Boston, MA'
     },
     'welcomeMessage': 'change',
     'skills': ['HTML', 'CSS', 'JavaScript'],
-    'biopic': 'change',
+    'biopic': 'images/fry.jpg',
     'display': function () {
+        var formattedHeaderRole = HTMLheaderRole.replace('%data%', bio.role);
+        $('#header').prepend(formattedHeaderRole);
 
+        var formattedHeaderName = HTMLheaderName.replace('%data%', bio.name);
+        $('#header').prepend(formattedHeaderName);
+
+        var formattedMobile = HTMLmobile.replace('%data%', bio.contacts.mobile);
+        $('#topContacts').append(formattedMobile);
+        $('#footerContacts').append(formattedMobile);
+
+        var formattedEmail = HTMLemail.replace('%data%', bio.contacts.email);
+        $('#topContacts').append(formattedEmail);
+        $('#footerContacts').append(formattedEmail);
+
+        var formattedGithub = HTMLgithub.replace('%data%', bio.contacts.github);
+        $('#topContacts').append(formattedGithub);
+        $('#footerContacts').append(formattedGithub);
+
+        var formattedLocation = HTMLlocation.replace('%data%', bio.contacts.location);
+        $('#topContacts').append(formattedLocation);
+        $('#footerContacts').append(formattedLocation);
+
+        var formattedBioPic = HTMLbioPic.replace('%data%', bio.biopic);
+        $('#header').append(formattedBioPic);
+
+        var formattedWelcomeMsg = HTMLwelcomeMsg.replace('%data%', bio.welcomeMessage);
+        $('#header').append(formattedWelcomeMsg);
+
+        $('#header').append(HTMLskillsStart);
+
+        bio.skills.forEach(function(skill) {
+            var formattedSkills = HTMLskills.replace('%data%', skill);
+            $('#skills').append(formattedSkills);
+        });
     }
 };
 
@@ -60,3 +93,5 @@ var projects = {
 
     }
 };
+
+bio.display();
